61ea5c7f96c0bed7a9fabfc7e3bdc3aedaff2bed
Shashankpandey09 <137607589+Shashankpandey09@users.noreply.github.com>
2024-09-02 17:31:30 -0800

    creating frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
-import SignUp from "./AuthComponents/SignUp";
+import SignUp from "./Pages/SignUp";
+import SignIn from "./Pages/SignIn";
+import Dashboard from "./Pages/Dashboard";
+import Send from "./Pages/Send";
 
 function App() {
   return <div>
     <Router>
       <Routes>
       <Route exact path="/" element={<SignUp/>}/>
+      <Route  path="/signIn" element={<SignIn/>}/>
+      <Route  path="/Dashboard" element={<Dashboard/>}/>
+      <Route  path="/Send" element={<Send/>}/>
       </Routes>
     </Router>
   </div>
